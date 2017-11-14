b639d02ae3a41ed149924daaf17eecbdf36fdd67
Swizec Teller <swizec@swizec.com>
2017-11-14 02:17:24 -0800

    Add FB pixel

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,62 +3,70 @@ import PropTypes from "prop-types";
 import Link from "gatsby-link";
 import Helmet from "react-helmet";
 
+import FBPixel from "react-facebook-pixel";
+
 import "bootstrap/dist/css/bootstrap.css";
 import "../css/bootstrap-override.css";
 import "../css/bootstrap-custom-utils.css";
 import "../css/salesbury-lilac.css";
 import "./index.css";
 
-const TemplateWrapper = ({ children }) => (
-    <div className="cheatsheet">
-        <Helmet>
-            <script async src="//assets.codepen.io/assets/embed/ei.js" />
-            <script async src="https://gumroad.com/js/gumroad.js" />
+FBPixel.init(714190382013726);
 
-            <meta name="author" content="Swizec Teller" />
-            <meta
-                name="description"
-                content="Interactive ES6, ES2016, and ES2017 cheatsheet."
-            />
-            <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1.0"
-            />
-            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+const TemplateWrapper = ({ children }) => {
+    FBPixel.pageView();
 
-            <meta property="og:url" content="https://es6cheatsheet.com" />
-            <meta
-                property="og:title"
-                content="Interactive ES6, ES2016, and ES2017 cheatsheet"
-            />
-            <meta
-                property="og:image"
-                content="https://es6cheatsheet.com/cover.png"
-            />
-            <meta
-                property="og:description"
-                content="This is an interactive ES6+ cheatsheet. 13 sections, 45 runnable code samples."
-            />
+    return (
+        <div className="cheatsheet">
+            <Helmet>
+                <script async src="//assets.codepen.io/assets/embed/ei.js" />
+                <script async src="https://gumroad.com/js/gumroad.js" />
 
-            <meta name="twitter:card" content="summary" />
-            <meta name="twitter:site" content="@swizec" />
-            <meta name="twitter:creator" content="@swizec" />
-            <meta
-                name="twitter:title"
-                content="Interactive ES6, ES2016, and ES2017 cheatsheet"
-            />
-            <meta
-                name="twitter:image"
-                content="https://es6cheatsheet.com/cover.png"
-            />
-            <meta
-                name="twitter:description"
-                content="This is an interactive ES6+ cheatsheet. 13 sections, 45 runnable code samples."
-            />
-        </Helmet>
-        {children()}
-    </div>
-);
+                <meta name="author" content="Swizec Teller" />
+                <meta
+                    name="description"
+                    content="Interactive ES6, ES2016, and ES2017 cheatsheet."
+                />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+                <meta property="og:url" content="https://es6cheatsheet.com" />
+                <meta
+                    property="og:title"
+                    content="Interactive ES6, ES2016, and ES2017 cheatsheet"
+                />
+                <meta
+                    property="og:image"
+                    content="https://es6cheatsheet.com/cover.png"
+                />
+                <meta
+                    property="og:description"
+                    content="This is an interactive ES6+ cheatsheet. 13 sections, 45 runnable code samples."
+                />
+
+                <meta name="twitter:card" content="summary" />
+                <meta name="twitter:site" content="@swizec" />
+                <meta name="twitter:creator" content="@swizec" />
+                <meta
+                    name="twitter:title"
+                    content="Interactive ES6, ES2016, and ES2017 cheatsheet"
+                />
+                <meta
+                    name="twitter:image"
+                    content="https://es6cheatsheet.com/cover.png"
+                />
+                <meta
+                    name="twitter:description"
+                    content="This is an interactive ES6+ cheatsheet. 13 sections, 45 runnable code samples."
+                />
+            </Helmet>
+            {children()}
+        </div>
+    );
+};
 
 TemplateWrapper.propTypes = {
     children: PropTypes.func
